168c799d8a05f80d881a453c14c5949aab4a5bc4
fwcd <fwcdmail@gmail.com>
2022-08-21 01:13:30 +0200

    Determine CGPoint relative to event target's bounds

--- Sources/MusicWebPlayground/Utils/CGPoint+MouseEvent.swift
+++ Sources/MusicWebPlayground/Utils/CGPoint+MouseEvent.swift
@@ -3,7 +3,10 @@ import JavaScriptKit
 
 extension CGPoint {
     init(mouseEvent event: JSObject) {
-        // TODO: Subtract origin position of target element
-        self.init(x: event.clientX.number!, y: event.clientY.number!)
+        let parent = event.target.getBoundingClientRect()
+        self.init(
+            x: event.clientX.number! - parent.left.number!,
+            y: event.clientY.number! - parent.top.number!
+        )
     }
 }
