a01aa53876f3125e23f0c307b19bbe82fecf23f6
fwcd <fwcdmail@gmail.com>
2022-08-21 02:08:57 +0200

    Request Web MIDI access

---
+++ Sources/MusicWebPlayground/ViewModel/MidiError.swift
@@ -0,0 +1,3 @@
+enum MidiError: Error {
+    case midiUnavailable
+}
--- Sources/MusicWebPlayground/ViewModel/MidiManager.swift
+++ Sources/MusicWebPlayground/ViewModel/MidiManager.swift
@@ -1,6 +1,30 @@
 import OpenCombine
+import JavaScriptKit
 
 /// Handles interaction with the Web MIDI API.
 class MidiManager: ObservableObject {
     @Published var midiAvailable: Bool = false
+
+    init() {
+        Task {
+            do {
+                try await initializeWebMidi()
+                midiAvailable = true
+            } catch MidiError.midiUnavailable {
+                print("Web MIDI is unavailable")
+            } catch {
+                print("Could not initialize Web MIDI: \(error)")
+            }
+        }
+    }
+
+    private func initializeWebMidi() async throws {
+        let navigator = JSObject.global.navigator
+        guard navigator.requestMIDIAccess.function != nil,
+              let midiPromise = navigator.requestMIDIAccess(navigator).object,
+              let midiAccess = try await JSPromise(midiPromise)?.value else {
+            throw MidiError.midiUnavailable
+        }
+        print("MIDI access: \(midiAccess)")
+    }
 }
