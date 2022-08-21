7ce679738c90fa59c27563375b421f893616b248
fwcd <fwcdmail@gmail.com>
2022-08-21 02:44:21 +0200

    Update activeMidiNotes based on statusKind

--- Sources/MusicWebPlayground/ViewModel/MidiManager.swift
+++ Sources/MusicWebPlayground/ViewModel/MidiManager.swift
@@ -31,7 +31,7 @@ class MidiManager: ObservableObject {
         
         // TODO: Continuously monitor for inputs? Is that needed?
         _ = midiAccess.inputs.forEach(JSClosure { entry in
-            _ = entry[0].addEventListener("midimessage", JSClosure { msg in
+            _ = entry[0].addEventListener("midimessage", JSClosure { [self] msg in
                 // TODO: Move parsing to a dedicated library
                 let buffer = msg[0].data.object!
                 let midiMsg = MidiMessage(
@@ -40,6 +40,14 @@ class MidiManager: ObservableObject {
                     data2: UInt8(buffer[2].number!)
                 )
                 print("Got \(midiMsg) (kind: \(midiMsg.statusKind.map { "\($0)".split(separator: ".").last! } ?? "?"))")
+                switch midiMsg.statusKind {
+                case .noteOn:
+                    activeMidiNotes.insert(Int(midiMsg.data1))
+                case .noteOff:
+                    activeMidiNotes.insert(Int(midiMsg.data1))
+                default:
+                    break
+                }
                 return .undefined
             })
             return .undefined
